a5cb19e858467a7929cd59800de992b5a6b9d58b
Sdmitr90 <Sdmitr90@mail.ru>
2022-01-23 20:14:51 +0300

    HomeWork_Lesson_4

--- resources/views/mysite/index.blade.php
+++ resources/views/mysite/index.blade.php
@@ -5,18 +5,25 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="{{asset('js/app.js')}}" defer></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>@section('title') Главная страница  @show</title>
 </head>
 <body>
-<div class="header">
-    @yield('menu')
-</div>
-<div class="content">
-    CONTENT
-    @yield('content')
-</div>
-<div class="footer">
-    FOOTER
+<div class="col-md-6">
+    <div class="header">
+        @include('blocks.menu')
+        <nr></nr>
+        @yield('menu')
+
+    </div>
+    <div class="content">
+        CONTENT
+        @yield('content')
+    </div>
+    <div class="footer">
+        FOOTER
+    </div>
 </div>
 </body>
 </html>
